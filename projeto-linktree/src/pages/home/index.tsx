0d2c080a881a54818759194080bdd2bb289c5e8e
Carolina Russi <carolinaferlarussi@gmail.com>
2025-04-13 20:51:49 -0300

    projeto linktree ajustes

--- projeto-linktree/src/pages/home/index.tsx
+++ projeto-linktree/src/pages/home/index.tsx
@@ -72,7 +72,7 @@ export function Home() {
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
       <h1 className="md:text-4xl text-3x1 font-bold text-white mt-20">
-        Página Home
+        DevLinks - Carolina
       </h1>
       <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇🏻</span>
 
